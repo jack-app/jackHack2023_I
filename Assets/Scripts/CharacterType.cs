541cf2d71093aabdb043ca6a0fda605644ba3cff
itoka229 <itoka229mori@gmail.com>
2023-05-04 01:30:05 +0900

    明日実装予定の機能を追加

---
+++ Assets/Scripts/CharacterType.cs
@@ -0,0 +1,15 @@
+public enum CharacterType
+{
+    BandMan,
+    CareerWoman,
+    Charao,
+    Gal,
+    Intelli,
+    Kanemochi,
+    Majime,
+    Ojosama,
+    OL,
+    Onesan,
+    Otaku,
+    OtonaOnesan,
+}
