e1312b548b4c21ab7f96e26f4c3fa7a37e78f6d5
hizumo <104607366+hizumo@users.noreply.github.com>
2023-05-03 23:41:28 +0900

    ScriptableObjectを関数に変更

--- Assets/Scripts/PieceManager.cs
+++ Assets/Scripts/PieceManager.cs
@@ -9,33 +9,37 @@ public class PieceManager : MonoBehaviour
     GameObject prefabi;
     Vector3 Positioni;
 
-    // ScriptableObjectを直接アタッチしたい場合は以下のコメントアウトを外してください。
-    //public EnemyStatusScriptableObject enemyStatusScriptableObject;
-
     // ScriptableObjectのパスを指定（ScriptableObjectを右クリック>copy path取得できます。）
     string filePath = "Assets/New Piece Status Scriptable Object.asset";
 
-    private void Start()
+    // テスト用にキーボードのスペースキーを押した時に関数を呼び出す
+    private void Update()
     {
-        // ScriptableObjectのパスを指定して、EnemyStatusScriptableObjectを取得する。
-        // 上のコメントアウトを外した場合は、こちらをコメントアウトしてください
-        PieceStatusScriptableObject pieceStatusScriptableObject = AssetDatabase.LoadAssetAtPath<PieceStatusScriptableObject>(filePath);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PieceStatusScriptableObject pieceStatusScriptableObject = AssetDatabase.LoadAssetAtPath<PieceStatusScriptableObject>(filePath);
+            CreateMyPieces(pieceStatusScriptableObject);
+        }
+    }
+
+    public void CreateMyPieces(PieceStatusScriptableObject pieceStatusScriptableObject)
+    {
 
         int listCount = pieceStatusScriptableObject.list.Count;//Listの要素数の取得 
-        Debug.Log("listCount:"+listCount);
+        Debug.Log("listCount:" + listCount);
         for (int i = 0; i < listCount; i++)
         {
             prefabi = pieceStatusScriptableObject.list[i].prefab;//prefabの取得
             Positioni = pieceStatusScriptableObject.list[i].Position;//Positionの取得
 
-
             Debug.Log("prefab:" + prefabi);
             Debug.Log("Positon:" + Positioni);
-
 
             // 新しいオブジェクトを生成する
             GameObject newObjecti = Instantiate(prefabi, Positioni, Quaternion.identity);
 
         }
+
     }
+
 }
