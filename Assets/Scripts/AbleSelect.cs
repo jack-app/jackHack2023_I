2c6dcf87657c3500170d65030b3daede73496583
itoka229 <itoka229mori@gmail.com>
2023-05-04 17:34:24 +0900

    制限範囲内でコマを指す機能を作成

--- Assets/Scripts/AbleSelect.cs
+++ Assets/Scripts/AbleSelect.cs
@@ -4,79 +4,29 @@ using UnityEngine;
 
 public class AbleSelect : MonoBehaviour
 {
-    private Clickmode clickmode;
     // Start is called before the first frame update
     void Start()
     {
-        clickmode = Clickmode.clickpiece;
         foreach (Transform child in transform)
         {
             child.gameObject.SetActive(false);
 
         }
-
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ActivateBoard()
     {
-        if (Input.GetMouseButtonDown(0)) // 左クリック
+        foreach (Transform child in transform)//子オブジェクトをアクティブにする
         {
-            if (clickmode == Clickmode.clickpiece)
-            {
-                Ray raypiece = Camera.main.ScreenPointToRay(Input.mousePosition); // Rayを生成
-                RaycastHit hitpiece;
-                if (Physics.Raycast(raypiece, out hitpiece, 10.0f)) // Rayを投射
-                {
-                    if (hitpiece.collider.CompareTag("Piece")) // タグを比較
-                    {
-                        foreach (Transform child in transform)//子オブジェクトをアクティブにする
-                        {
-                            child.gameObject.SetActive(true);
-                            clickmode = Clickmode.clickboard;
-
-                        }
-                    }
-                }
-            }
-            else if (clickmode == Clickmode.clickboard)
-            {
-                Ray rayboard = Camera.main.ScreenPointToRay(Input.mousePosition); // Rayを生成
-                RaycastHit hitboard;
-                if (Physics.Raycast(rayboard, out hitboard, 10.0f)) // Rayを投射
-                {
-                    if (hitboard.collider.CompareTag("Board")) // タグを比較
-                    {
-                        foreach (Transform child in transform)//子オブジェクトを非アクティブにする
-                        {
-                            child.gameObject.SetActive(false);
-                            clickmode = Clickmode.clickpiece;
-                        }
-                    }
-                    else
-                    {
-                        foreach (Transform child in transform)//子オブジェクトを非アクティブにする
-                        {
-                            child.gameObject.SetActive(false);
-                            clickmode = Clickmode.clickpiece;
-                        }
-                    }
-                }
-                else
-                {
-                    foreach (Transform child in transform)//子オブジェクトを非アクティブにする
-                    {
-                        child.gameObject.SetActive(false);
-                        clickmode = Clickmode.clickpiece;
-                    }
-                }
-            }
-            
+            child.gameObject.SetActive(true);
         }
     }
-    public enum Clickmode
+
+    public void InActivateBoard()
     {
-        clickpiece,
-        clickboard,
+        foreach (Transform child in transform)//子オブジェクトを非アクティブにする
+        {
+            child.gameObject.SetActive(false);
+        }
     }
 }
